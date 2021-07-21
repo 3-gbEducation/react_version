239747d1b6443469e5f000e7e7c89724f1349130
Pradeepa Seelam <39674626+pradeepa45@users.noreply.github.com>
2021-07-21 23:14:16 +0530

    logo and titles on browser tab

---
+++ src/Pages/LatestUpdate.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LatestUpadate = () => {
+    document.title ="3-GB - Updates"
+
+    return (
+        <div>
+            <h1>Latest Update page </h1>
+        </div>
+    )
+}
+
+export default LatestUpadate
